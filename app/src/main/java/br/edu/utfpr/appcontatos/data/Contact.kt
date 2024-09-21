17d12a30145c56f4019440292a3a42c38466fcb7
Jorge Gabriel Rodrigues <jorgegabriel99h@gmail.com>
2024-09-21 08:29:17 -0300

    feat(components change): scaffold

---
+++ app/src/main/java/br/edu/utfpr/appcontatos/data/Contact.kt
@@ -0,0 +1,12 @@
+package br.edu.utfpr.appcontatos.data
+
+data class Contact(
+    val id: Int = 0,
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = "",
+    val email: String = "",
+    val isFavorite: Boolean = false
+) {
+    val fullName get(): String = "$firstName $lastName".trim()
+}
